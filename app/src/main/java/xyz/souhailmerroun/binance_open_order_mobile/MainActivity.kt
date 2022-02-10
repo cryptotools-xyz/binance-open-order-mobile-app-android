28c787c593bc0c6eac6f97ddd981587883e6275d
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-02-10 22:15:41 +0100

    Empty Activity Project

---
+++ app/src/main/java/xyz/souhailmerroun/binance_open_order_mobile/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.souhailmerroun.binance_open_order_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
