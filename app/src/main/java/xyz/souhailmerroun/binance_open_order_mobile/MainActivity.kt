1a156c0080f8f1b181cc0ae6e488bb559ba7c19f
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-02-10 22:54:12 +0100

    Adding WebView with load url

--- app/src/main/java/xyz/souhailmerroun/binance_open_order_mobile/MainActivity.kt
+++ app/src/main/java/xyz/souhailmerroun/binance_open_order_mobile/MainActivity.kt
@@ -2,10 +2,14 @@ package xyz.souhailmerroun.binance_open_order_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("https://www.google.com")
     }
 }
